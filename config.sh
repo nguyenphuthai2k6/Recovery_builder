8186dc29e7abf152ba92211edb4ec16a15529013
nguyenphuthai2k6 <nguyenphuthai2k6@gmail.com>
2023-11-05 07:50:00 +0700

    config.sh

--- config.sh
+++ config.sh
@@ -7,8 +7,8 @@ export BRANCH="twrp-11" # the branch of manifest
 
 # about your device
 export DEVICE="willowpro" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
-export DT_BRANCH="twrp" # device tree branch
+export DT_LINK="https://github.com/nguyenphuthai2k6/android_device_vsmart_star4.git" # device tree link
+export DT_BRANCH="main" # device tree branch
 export VENDOR="vsmart" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="twrp" # recovery type: omni, twrp, something else
